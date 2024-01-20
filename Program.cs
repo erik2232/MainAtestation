d1fe7c932e95febb39b6a4729ce68e0f0b21e7b3
erik2232 <eriksahakyan62@gmail.com>
2024-01-20 21:04:22 +0400

    Mission Complete

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿string s= "gfgf6254";
-Console.WriteLine($"{s.Length}");
+﻿string[] Array = {"Moscow","Yerevan","5","Paris"};
+int newArrayLength = 0;
+
+for (int i = 0; i < Array.Length; i++)
+{
+    if(Array[i].Length>=3)
+    {
+      newArrayLength++;
+
+    }
+    
+}
+string[] NewArray = new string[newArrayLength];
+int newArrayIndex = 0;
+for (int i = 0; i <Array.Length; i++)
+{
+    if(Array[i].Length>=3)
+    {
+      NewArray[newArrayIndex] = Array[i];
+      newArrayIndex++;
+    }
+    
+
+}
+Console.WriteLine(string.Join(',',NewArray));
